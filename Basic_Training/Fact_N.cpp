88662d1d123f2ffc18872d33bf7b69eacd15bad3
pkailashinventyv <kailashpinventyv@gmail.com>
2023-12-12 11:27:05 +0530

    Add Upto N

---
+++ Basic_Training/Fact_N.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int fact(int n){
+    int res=1;
+    for(int i=1;i<=n;i++){
+        res*=i;
+    }
+    return res;
+}
+int main(){
+    int n;
+    cout<<"Enter N : ";
+    cin>>n;
+    for(int i=1;i<2*n;i+=2){
+        cout<<fact(i)<<" ";
+    }
+    return 0;
+}
