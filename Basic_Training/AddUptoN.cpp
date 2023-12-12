683e2486d646c3101a041f0b61741550d3bfabb3
pkailashinventyv <kailashpinventyv@gmail.com>
2023-12-12 11:13:37 +0530

    Add Upto N Numbers

---
+++ Basic_Training/AddUptoN.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,res=0;
+    cout<<"Enter N : ";
+    cin>>n;
+    for(int i=1;i<=n;i++){
+        res+=i;
+    }
+    cout<<res;
+    return 0;
+}
