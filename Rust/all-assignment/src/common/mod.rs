9d6f6d4ad29ddd4268068bc24d76ef09b516a6eb
pkailashinventyv <kailashpinventyv@gmail.com>
2024-01-31 17:44:05 +0530

    Update Structure

--- Rust/all-assignment/src/common/mod.rs
+++ Rust/all-assignment/src/common/mod.rs
@@ -3,7 +3,6 @@ use serde::{Serialize,Deserialize};
 
 #[derive(Debug,Deserialize,Serialize)]
 
-
 pub struct Employee{
     pub name:String,
     pub age:u8,
@@ -47,29 +46,83 @@ pub struct Student {
     pub grade: Option<String>,
 }
 
-#[derive(Debug)]
+#[derive(Debug,Serialize)]
 pub struct Cell{
-    pub width:u8,
-    pub height:u8,
-    pub value:u8
+    pub width:f32,
+    pub height:f32,
+    pub value:String
 }
 
-#[derive(Debug)]
+#[derive(Debug,Serialize)]
 
 pub struct Row{
-    pub row_height:u8,
-    pub tot_cells:u8,
+    pub row_height:f32,
+    pub tot_cells:f32,
     pub row_cells:Vec<Cell>,
-    pub row_width:u8
+    pub row_width:f32
 }
 
 
-#[derive(Debug)]
+#[derive(Debug,Serialize)]
 
 
 pub struct Table{
-    pub total_height:u8,
-    pub total_width:u8,
+    pub total_height:f32,
+    pub total_width:f32,
     pub all_row:Vec<Row>,
-    pub no_row:u8,
+    pub no_row:f32,
 }
+
+
+#[derive(Debug, Deserialize)]
+
+pub struct headerRow{
+    pub fontSize:i32,
+    pub title:Vec<String>
+}
+#[derive(Debug, Deserialize)]
+
+pub struct dataRows{
+    pub fontSize:i32,
+    pub rows:Vec<Vec<String>>
+}
+#[derive(Debug, Deserialize)]
+
+pub struct tableData{
+    pub headerRow:headerRow,
+    pub dataRows:dataRows,
+    pub pageWidth:i32
+}
+
+#[derive(Debug,Serialize)]
+pub enum data_type{
+    HEADER,
+    RECORD
+}
+
+#[derive(Debug,Serialize)]
+pub struct tCell{
+    pub data:String
+}
+
+#[derive(Debug,Serialize)]
+pub struct tRow{
+    pub cell_width:f32,
+    pub total_cells:i32,
+    pub row_width:f32,
+    pub row_height:f32,
+    pub row_type:data_type,
+    pub row_cells:Vec<tCell>
+}
+
+#[derive(Debug,Serialize)]
+pub struct tTable{
+    pub tab_height: f32,
+    pub tab_width: f32,
+    pub all_rows:Vec<tRow>,
+    pub tot_rows:i32
+}
+
+
+
+
